428cc4ddae080c6e94ce9465ec90f1ec823e51d8
mastercoks <matheusbdo1@gmail.com>
2017-12-16 20:15:58 -0300

    Jogo com executavel

---
+++ Assets/Scripts/ExecutaPassos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecutaPassos : MonoBehaviour {
+
+    [SerializeField] private AudioClip[] grass_steps;
+    [SerializeField] private AudioSource step_source;
+    private int aux_step = 1;
+
+    public void PlayStep() {
+        step_source.Play();
+        aux_step = (aux_step + 1) % 2;
+    }
+
+    public void PlayGrassStep() {
+        step_source.clip = grass_steps[aux_step];
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
